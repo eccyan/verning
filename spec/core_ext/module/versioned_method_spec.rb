e2af40c5e3d60e2bfd701f6983c13c3d83db5467
sato_daisuke <sato_daisuke@cyberagent.co.jp>
2014-03-26 14:26:08 +0900

    added rspec

---
+++ spec/core_ext/module/versioned_method_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Module do
+  describe "when matches the conditions"do
+    it "aliased target method" do
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'versioned' 
+
+RSpec.configure do |config|
+end
